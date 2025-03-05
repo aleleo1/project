6a0a27001367588f0a1abd13a7f4dac67d570bbb
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2025-03-05 13:48:58 +0100

    first

---
+++ src/components/Chart.tsx
@@ -0,0 +1,9 @@
+
+export default function Chart(p: any) {
+    return (
+          <div style="width: 50vw;"><div id="chart" class="chart-container">
+          </div>
+          </div>        
+      );
+}
+
